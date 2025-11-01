7fc4cf5696ca00aa8e77c0a7b0c898668ab15497
Mark Ngo <mark.ngo203@gmail.com>
2025-11-01 16:05:24 -0400

    Change deafult gRPC port.

--- app/config.py
+++ app/config.py
@@ -9,4 +9,4 @@ class Config:
     DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/auction_db")
 
     GRPC_HOST = os.getenv("GRPC_HOST", "localhost")
-    GRPC_PORT = os.getenv("GRPC_PORT", 50051)
+    GRPC_PORT = os.getenv("GRPC_PORT", 50054)
